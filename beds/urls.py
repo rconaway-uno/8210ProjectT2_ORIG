4a73025465d9b9566dfcde07304210da06d43ec9
stutiraut <sraut@unomaha.edu>
2019-03-24 22:54:37 -0500

    manage users and manage events page added for admin

--- beds/urls.py
+++ beds/urls.py
@@ -10,20 +10,6 @@ from django.contrib.auth import views as auth_views
 app_name = 'beds'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    url(r'^home/$', views.home, name='home'),
-
-    url(r'^admin_login/', views.admin_home, name='adminlogin'),
-    url(r'^nurse_login/', views.nurse_home, name='nurselogin'),
-    path('admin_home', views.admin_home, name='admin_home'),
-    path('nurse_home', views.nurse_home, name='nurse_home'),
-#change password urls
-    path('accounts/password_change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
-    path('accounts/password_change/done/',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
-    path('password_reset/',	auth_views.PasswordResetView.as_view(),	name='password_reset'),
-    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),	name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',	auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('reset/done/',	auth_views.PasswordResetCompleteView.as_view(),	name='password_reset_complete'),
 
 ]
 
